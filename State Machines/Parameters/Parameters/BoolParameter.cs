9bb2ffeff0e2ccc8e9cddb1ef230bd2937d53290
reagancheatham <reagancheatham@gmail.com>
2025-07-30 18:30:36 -0500

    Remove old stuff

--- State Machines/Parameters/Parameters/BoolParameter.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace Shears.StateMachines
-{
-    public class BoolParameter : Parameter<bool>
-    {
-        public BoolParameter(string name) : base(name) { }
-    }
-}
