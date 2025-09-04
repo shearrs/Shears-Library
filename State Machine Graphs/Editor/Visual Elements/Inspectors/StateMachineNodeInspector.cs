5a22b531ce334fab8502b2875ff342b3761e2fff
reagancheatham <reagancheatham@gmail.com>
2025-09-03 23:21:04 -0500

    Add requirement list to state inspectors

--- State Machine Graphs/Editor/Visual Elements/Inspectors/StateMachineNodeInspector.cs
+++ State Machine Graphs/Editor/Visual Elements/Inspectors/StateMachineNodeInspector.cs
@@ -1,12 +1,21 @@
 using Shears.GraphViews;
+using System;
+using UnityEditor.UIElements;
+using UnityEngine;
 using UnityEngine.UIElements;
 
 namespace Shears.StateMachineGraphs.Editor
 {
     public class StateMachineNodeInspector : GraphNodeInspector<StateMachineNodeData>
     {
+        private readonly VisualElement referenceHintContainer;
+
         public StateMachineNodeInspector(GraphData graphData) : base(graphData)
         {
+            referenceHintContainer = new VisualElement()
+            {
+                name = "Reference Hint Container"
+            };
         }
 
         protected override void BuildInspector(VisualElement nameField, VisualElement transitions)
@@ -15,7 +24,32 @@ namespace Shears.StateMachineGraphs.Editor
 
             Add(nameField);
             Add(new StateSelector(stateTypeProp));
+            Add(referenceHintContainer);
             Add(transitions);
+
+            AddReferenceHints();
+            referenceHintContainer.TrackPropertyValue(stateTypeProp, (prop) => AddReferenceHints());
+        }
+
+        private void AddReferenceHints()
+        {
+            referenceHintContainer.Clear();
+
+            if (nodeData.StateType == SerializableSystemType.Empty)
+                return;
+
+            if (!typeof(IStateInjectable).IsAssignableFrom(nodeData.StateType))
+                return;
+
+            var stateInstance = Activator.CreateInstance(nodeData.StateType) as IStateInjectable;
+            var injectableTypes = stateInstance.GetInjectableTypes();
+
+            foreach (var type in injectableTypes)
+            {
+                var label = new Label($"Requires: {StringUtil.PascalSpace(type.Name)}");
+
+                referenceHintContainer.Add(label);
+            }
         }
     }
 }
--- State Machine Graphs/Editor/Visual Elements/Inspectors/StateNodeInspector.cs
+++ State Machine Graphs/Editor/Visual Elements/Inspectors/StateNodeInspector.cs
@@ -1,12 +1,20 @@
 using Shears.GraphViews;
+using System;
+using UnityEditor.UIElements;
 using UnityEngine.UIElements;
 
 namespace Shears.StateMachineGraphs.Editor
 {
     public class StateNodeInspector : GraphNodeInspector<StateNodeData>
     {
+        private readonly VisualElement referenceHintContainer;
+
         public StateNodeInspector(GraphData graphData) : base(graphData)
         {
+            referenceHintContainer = new VisualElement()
+            {
+                name = "Reference Hint Container"
+            };
         }
 
         protected override void BuildInspector(VisualElement nameField, VisualElement transitions)
@@ -15,7 +23,33 @@ namespace Shears.StateMachineGraphs.Editor
 
             Add(nameField);
             Add(new StateSelector(stateTypeProp));
+            Add(referenceHintContainer);
             Add(transitions);
+
+            AddReferenceHints();
+            referenceHintContainer.Unbind();
+            referenceHintContainer.TrackPropertyValue(stateTypeProp, (prop) => AddReferenceHints());
+        }
+
+        private void AddReferenceHints()
+        {
+            referenceHintContainer.Clear();
+
+            if (nodeData.StateType == SerializableSystemType.Empty)
+                return;
+
+            if (!typeof(IStateInjectable).IsAssignableFrom(nodeData.StateType))
+                return;
+
+            var stateInstance = Activator.CreateInstance(nodeData.StateType) as IStateInjectable;
+            var injectableTypes = stateInstance.GetInjectableTypes();
+
+            foreach (var type in injectableTypes)
+            {
+                var label = new Label($"Requires: {StringUtil.PascalSpace(type.Name)}");
+
+                referenceHintContainer.Add(label);
+            }
         }
     }
 }
