0db8f01d027335508a2770d7117c57adf818b029
reagancheatham <reagancheatham@gmail.com>
2025-09-09 13:56:56 -0500

    Add menu tools

---
+++ Common/Editor/MenuTools.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace Shears.Editor
+{
+    public class MenuTools : MonoBehaviour
+    {
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
