ad77880a55c1e806c49069ed64cee43414df6af5
reagancheatham <reagancheatham@gmail.com>
2025-09-09 19:18:56 -0500

    Refactor and document timer to be readable mid timing

--- Common/Util/Timer.cs
+++ Common/Util/Timer.cs
@@ -5,16 +5,27 @@ using UnityEngine;
 
 namespace Shears
 {
+    /// <summary>
+    /// A simple timer class.<br/><br/>
+    /// 
+    /// Features:<br/>
+    /// - Dynamic or preset time<br/>
+    /// - <see cref="SafeAwaitable"/> timer<br/>
+    /// - Completion callbacks
+    /// </summary>
     [Serializable]
     public class Timer
     {
-        [SerializeField] private float time = 1;
+        [SerializeField] private float time = 1f;
+        [SerializeField] private float currentTime = 0.0f;
         [SerializeField, ReadOnly] private bool isDone = true;
         
         private CancellationTokenSource tokenSource;
         private readonly List<Action> onComplete = new();
 
         public float Time { get => time; set => time = value; }
+        public float CurrentTime => currentTime;
+        public float Percentage => CurrentTime / Time;
         public bool IsDone => isDone;
 
         public Timer() { }
@@ -24,8 +35,19 @@ namespace Shears
             this.time = time;
         }
 
+        /// <summary>
+        /// Starts the timer with the timer's default time (in seconds).<br/><br/>
+        /// Default time is set in 3 ways:<br/>
+        /// 1. Passed in the constructor<br/>
+        /// 2. Set with <see cref="Time"/><br/>
+        /// 3. Default set to 1.0
+        /// </summary>
         public void Start() => Start(time);
 
+        /// <summary>
+        /// Starts the timer with a passed time (in seconds).
+        /// </summary>
+        /// <param name="time">The time in seconds for the timer.</param>
         public void Start(float time)
         {
             if (!isDone)
@@ -37,6 +59,9 @@ namespace Shears
             RunAsync(time, tokenSource.Token);
         }
 
+        /// <summary>
+        /// Stops the timer if it is running. Does not call completion callbacks.
+        /// </summary>
         public void Stop()
         {
             if (isDone)
@@ -46,18 +71,30 @@ namespace Shears
             isDone = true;
         }
 
+        /// <summary>
+        /// Stops and starts the timer.
+        /// </summary>
         public void Restart()
         {
             Stop();
             Start();
         }
 
+        /// <summary>
+        /// Adds a callback action for when the timer completes.<br/>
+        /// These actions are not cleared and will be called every time the timer completes.
+        /// </summary>
+        /// <param name="action">The callback to add.</param>
         public void AddOnComplete(Action action)
         {
             if (action != null && !onComplete.Contains(action))
                 onComplete.Add(action);
         }
 
+        /// <summary>
+        /// Removes a callback action from the completion list.
+        /// </summary>
+        /// <param name="action">The callback to remove.</param>
         public void RemoveOnComplete(Action action)
         {
             if (action != null)
@@ -67,8 +104,14 @@ namespace Shears
         private async void RunAsync(float time, CancellationToken token)
         {
             isDone = false;
+            currentTime = 0.0f;
 
-            await SafeAwaitable.WaitForSecondsAsync(time, token);
+            while (currentTime < time)
+            {
+                currentTime += UnityEngine.Time.deltaTime;
+
+                await SafeAwaitable.NextFrameAsync(token);
+            }
 
             isDone = true;
 
