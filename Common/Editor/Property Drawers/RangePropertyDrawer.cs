6eb32c1d668f8913c11463dc1ebab8fb80d72076
reagancheatham <reagancheatham@gmail.com>
2025-09-23 13:50:46 -0500

    Add range

---
+++ Common/Editor/Property Drawers/RangePropertyDrawer.cs
@@ -0,0 +1,55 @@
+using System;
+using UnityEditor;
+using UnityEditor.UIElements;
+using UnityEngine.UIElements;
+
+namespace Shears.Editor
+{
+    [CustomPropertyDrawer(typeof(Range<>), true)]
+    public class RangePropertyDrawer : PropertyDrawer
+    {
+        public override VisualElement CreatePropertyGUI(SerializedProperty property)
+        {
+            var root = new VisualElement();
+            root.AddStyleSheet(ShearsStyles.InspectorStyles);
+            root.AddToClassList(ShearsStyles.DarkContainerClass);
+
+            root.style.marginTop = 2;
+            root.style.marginBottom = 2;
+
+            var minProp = property.FindPropertyRelative("min");
+            var maxProp = property.FindPropertyRelative("max");
+
+            string title = property.displayName;
+
+            var header = VisualElementUtil.CreateHeader(title);
+            var minField = new PropertyField(minProp);
+            var maxField = new PropertyField(maxProp);
+
+            header.style.marginTop = 2;
+
+            root.AddAll(header, minField, maxField);
+
+            void enforceMinMax(SerializedPropertyChangeEvent evt)
+            {
+                if (minProp.boxedValue is IComparable minValue && maxProp.boxedValue is IComparable maxValue)
+                {
+                    if (minValue.CompareTo(maxValue) > 0)
+                    {
+                        if (evt.changedProperty == minProp)
+                            maxProp.boxedValue = minValue;
+                        else
+                            minProp.boxedValue = maxValue;
+
+                        minProp.serializedObject.ApplyModifiedProperties();
+                    }
+                }
+            }
+
+            minField.RegisterValueChangeCallback(enforceMinMax);
+            maxField.RegisterValueChangeCallback(enforceMinMax);
+
+            return root;
+        }
+    }
+}
---
+++ Common/Util/Extension Types/Range.cs
@@ -0,0 +1,65 @@
+using System;
+using UnityEngine;
+
+namespace Shears
+{
+    [Serializable]
+    public struct Range<T> : IComparable where T : IComparable
+    {
+        [SerializeField] private T min;
+        [SerializeField] private T max;
+
+        public readonly T Min => min;
+        public readonly T Max => max;
+
+        public Range(T min, T max)
+        {
+            if (min.CompareTo(max) > 0)
+                max = min;
+
+            this.min = min;
+            this.max = max;
+        }
+
+        /// <summary>
+        /// Compares the max values of two ranges.
+        /// </summary>
+        /// <param name="obj">The other range to compare to.</param>
+        /// <returns>The CompareTo value of both range's max values.</returns>
+        /// <exception cref="ArgumentException"></exception>
+        public int CompareTo(object obj)
+        {
+            if (obj is not Range<T> other)
+                throw new ArgumentException("Object is not a Range of the same type.");
+
+            int comparison = max.CompareTo(other.max);
+
+            return comparison;
+        }
+
+        /// <summary>
+        /// Returns a random value within the range. Only supported for int and float types.
+        /// </summary>
+        /// <returns>A random int or float value</returns>
+        /// <exception cref="NotSupportedException"></exception>
+        public readonly T Random()
+        {
+            if (typeof(T) == typeof(int))
+            {
+                int minValue = Convert.ToInt32(min);
+                int maxValue = Convert.ToInt32(max);
+
+                return (T)(object)UnityEngine.Random.Range(minValue, maxValue + 1);
+            }
+            else if (typeof(T) == typeof(float))
+            {
+                float minValue = Convert.ToSingle(min);
+                float maxValue = Convert.ToSingle(max);
+
+                return (T)(object)UnityEngine.Random.Range(minValue, maxValue);
+            }
+            else
+                throw new NotSupportedException("Random method is only supported for int and float types.");
+        }
+    }
+}
