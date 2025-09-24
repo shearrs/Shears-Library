c5e67dbfec54fdbd778eb2722dee747df276fa8a
reagancheatham <reagancheatham@gmail.com>
2025-09-23 21:46:50 -0500

    Fix world rect calculation

--- Common/Util/RectTransformExtensions.cs
+++ Common/Util/RectTransformExtensions.cs
@@ -5,32 +5,17 @@ namespace Shears
 {
     public static class RectTransformExtensions
     {
-        // From Alenya: https://discussions.unity.com/t/convert-recttransform-rect-to-rect-world/153391/5
+        // From Alenya: https://discussions.unity.com/t/convert-recttransform-rect-to-rect-world/153391/4
         public static Rect GetWorldRect(this RectTransform transform)
         {
-            var minX = float.MaxValue;
-            var minY = float.MaxValue;
-            var maxX = float.MinValue;
-            var maxY = float.MinValue;
-
             var pool = ArrayPool<Vector3>.Shared;
             var corners = pool.Rent(4);
             transform.GetWorldCorners(corners);
 
-            for (var i = 0; i < 4; i++)
-            {
-                var corner = corners[i];
-                minX = Mathf.Min(corner.x, minX);
-                minY = Mathf.Min(corner.y, minY);
-                maxX = Mathf.Max(corner.x, maxX);
-                maxY = Mathf.Max(corner.y, maxY);
-            }
-
+            var rect = new Rect(corners[0], corners[2] - corners[0]);
             pool.Return(corners);
 
-            var position = new Vector2(minX, minY);
-            var size = new Vector2(maxX - minX, maxY - minY);
-            return new Rect(position, size);
+            return rect;
         }
     }
 }
