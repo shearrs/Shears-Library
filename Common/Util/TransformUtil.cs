fc4080bcb8a157c0060154549897c63e3ce293b2
reagancheatham <reagancheatham@gmail.com>
2025-10-01 13:51:11 -0500

    Add transform util

---
+++ Common/Util/TransformUtil.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace Shears
+{
+    public static class TransformUtil
+    {
+        public static void SetLayerOnAllChildren(this Transform transform, int layer)
+        {
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                var child = transform.GetChild(i);
+
+                child.gameObject.layer = layer;
+
+                SetLayerOnAllChildren(child, layer);
+            }
+        }
+    }
+}
