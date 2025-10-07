fc42027aa69c64f527f0de5bf39432f088444eeb
reagancheatham <reagancheatham@gmail.com>
2025-10-07 17:08:27 -0500

    Add custom toString func

--- Common/Util/CollectionUtil.cs
+++ Common/Util/CollectionUtil.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,13 +10,16 @@ namespace Shears
     {
         private static readonly StringBuilder stringBuilder = new();
 
-        public static string ToCollectionString<T>(this IReadOnlyCollection<T> collection)
+        public static string ToCollectionString<T>(this IReadOnlyCollection<T> collection, Func<T, string> toString = null)
         {
             stringBuilder.Clear();
 
             for (int i = 0; i < collection.Count; i++)
             {
-                stringBuilder.Append(collection.ElementAt(i).ToString());
+                if (toString != null)
+                    stringBuilder.Append(toString(collection.ElementAt(i)));
+                else
+                    stringBuilder.Append(collection.ElementAt(i).ToString());
 
                 if (i < collection.Count - 1)
                     stringBuilder.Append(", ");
