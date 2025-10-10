aedb3bcb9f5242bd29b423d75d3322a8971eb473
reagancheatham <reagancheatham@gmail.com>
2025-10-10 00:08:27 -0500

    Add HitBox3DEditor

---
+++ Hit Detection/Editor/3D/HitBox3DEditor.cs
@@ -0,0 +1,17 @@
+using UnityEditor;
+using UnityEngine;
+
+namespace Shears.HitDetection.Editor
+{
+    [CustomEditor(typeof(HitBox3D))]
+    public class HitBox3DEditor : UnityEditor.Editor
+    {
+        [MenuItem("CONTEXT/HitBox3D/Reset Gizmo Settings")]
+        private static void ResetGizmoSettings(MenuCommand command)
+        {
+            var hitbox = (HitBox3D)command.context;
+
+            hitbox.ResetGizmoSettings();
+        }
+    }
+}
