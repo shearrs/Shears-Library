f487ba0f0e0b63e2fc56c7b262daf0f0d244f085
reagancheatham <reagancheatham@gmail.com>
2025-10-24 00:28:20 -0500

    Add selectbase

---
+++ Common/Util/SelectBase.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace Shears
+{
+    public class SelectBase : MonoBehaviour
+    {
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
