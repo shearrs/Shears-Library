63199a64dd5e031442ca881c92eeec4e24cfe523
reagancheatham <reagancheatham@gmail.com>
2025-12-18 20:30:34 -0600

    Add gameobject version

--- Common/Util/TransformUtil.cs
+++ Common/Util/TransformUtil.cs
@@ -4,6 +4,9 @@ namespace Shears
 {
     public static class TransformUtil
     {
+        public static void SetLayerOnAllChildren(this GameObject gameObject, int layer)
+            => SetLayerOnAllChildren(gameObject.transform, layer);
+
         public static void SetLayerOnAllChildren(this Transform transform, int layer)
         {
             for (int i = 0; i < transform.childCount; i++)
