613f82287f715488ab284e53507016c6a5ae6aef
reagancheatham <reagancheatham@gmail.com>
2025-12-23 02:29:36 -0600

    Suppress warnings

--- Common/Util/Attributes/AutoAttribute.cs
+++ Common/Util/Attributes/AutoAttribute.cs
@@ -1,11 +1,10 @@
+using JetBrains.Annotations;
 using System;
-using System.Diagnostics.CodeAnalysis;
-using UnityEngine;
 
 namespace Shears
 {
     [AttributeUsage(AttributeTargets.Field, Inherited = true)]
-    [SuppressMessage("Style", "IDE0044:Make field readonly", Justification = "Assigned automatically in Awake()")]
+    [MeansImplicitUse]
     public class AutoAttribute : Attribute
     {
     }
