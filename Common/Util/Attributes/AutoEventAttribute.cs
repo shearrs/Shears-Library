0cdf26db320e429d2ddaad365ba390a54f241199
reagancheatham <reagancheatham@gmail.com>
2025-12-23 04:45:59 -0600

    Add auto events

---
+++ Common/Util/Attributes/AutoEventAttribute.cs
@@ -0,0 +1,24 @@
+using System;
+using UnityEngine;
+
+namespace Shears
+{
+    /// <summary>
+    /// Fields marked with this attribute will have the specified event automatically subscribed to the specified callback method in generated OnEnable and OnDisable methods.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Field, Inherited = true)]
+    public class AutoEventAttribute : Attribute
+    {
+        private readonly string eventName;
+        private readonly string callbackName;
+
+        public string EventName => eventName;
+        public string CallbackName => callbackName;
+
+        public AutoEventAttribute(string eventName, string callbackName)
+        {
+            this.eventName = eventName;
+            this.callbackName = callbackName;
+        }
+    }
+}
