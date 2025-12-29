9b5e6e6660031eeedd30d19e2284d28bda25b1ff
reagancheatham <reagancheatham@gmail.com>
2025-12-29 03:23:56 -0600

    Change load request into a class

--- Loading/LoadRequest.cs
+++ Loading/LoadRequest.cs
@@ -1,20 +1,19 @@
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 namespace Shears.Loading
 {
     [System.Serializable]
-    public struct LoadRequest
+    public class LoadRequest
     {
         [SerializeField] private bool opensLoadingScreen;
         [SerializeField] private bool pausesGame;
         [SerializeField] private LoadAction[] actions;
 
-        public bool OpensLoadingScreen { readonly get => opensLoadingScreen; set => opensLoadingScreen = value; }
-        public bool PausesGame { readonly get => pausesGame; set => pausesGame = value; }
+        public bool OpensLoadingScreen { get => opensLoadingScreen; set => opensLoadingScreen = value; }
+        public bool PausesGame { get => pausesGame; set => pausesGame = value; }
 
-        public readonly IReadOnlyList<LoadAction> Actions => actions;
+        public IReadOnlyList<LoadAction> Actions => actions;
 
         public LoadRequest(params LoadAction[] actions)
         {
