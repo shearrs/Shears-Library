db68072b76c1677723595ee4cf4d25d3ed7bf3e4
reagancheatham <reagancheatham@gmail.com>
2026-03-23 23:57:51 -0500

    Make int parameter serializable

--- State Machine Graphs/Runtime/Parameters/IntParameter.cs
+++ State Machine Graphs/Runtime/Parameters/IntParameter.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 namespace Shears.StateMachineGraphs
 {
+    [System.Serializable]
     public class IntParameter : Parameter<int>
     {
         public IntParameter(IntParameterData data) : base(data)
