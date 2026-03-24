8c6150168195c22fe4085153bd88838f87ed6d53
reagancheatham <reagancheatham@gmail.com>
2026-03-23 21:42:19 -0500

    Make empty comparison data serializable

--- State Machine Graphs/Runtime/Data/Parameters/Comparisons/EmptyComparisonData.cs
+++ State Machine Graphs/Runtime/Data/Parameters/Comparisons/EmptyComparisonData.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 namespace Shears.StateMachineGraphs
 {
+    [System.Serializable]
     public class EmptyComparisonData : ParameterComparisonData
     {
         public override ParameterComparison CreateComparison(Parameter parameter) => null;
